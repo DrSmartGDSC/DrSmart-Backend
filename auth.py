70d1547346d6f3c6002b9852d68ab02346ae2ee9
mowafy001 <wafy123445@gmail.com>
2022-02-09 18:36:04 +0200

    rename

---
+++ auth.py
@@ -0,0 +1,23 @@
+from time import timezone
+import jwt
+import os
+from datetime import datetime, timezone
+
+ACCESS_SECRET = os.environ.get('ACCESS_SECRET')
+
+def create_access_token(payload):
+    payload['exp'] = datetime.now(tz=timezone.utc)
+    
+    token = jwt.encode(payload,  ACCESS_SECRET, alogrithem='HS256')
+    
+
+    return token
+
+def verify_access_token(token):
+    try:
+        payload = jwt.decode(token, ACCESS_SECRET)
+    except Exception as e:
+        print(str(e))
+        return False
+
+    return payload
